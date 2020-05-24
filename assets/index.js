6b35ca2e2fbc6427ed28e21af489bdb36eaec715
Alfredo Morales Sánchez <bluesmoss@gmail.com>
2020-05-24 18:42:05 -0500

    feature(MediaPlayer):  Intersection observer & visibility change

--- assets/index.js
+++ assets/index.js
@@ -1,12 +1,14 @@
 import MediaPlayer from './MediaPlayer.js';
 import AutoPlay from './plugins/Autoplay.js';
+import AutoPause from './plugins/AutoPause.js';
 
 const video = document.querySelector('video');
 const btnPlay = document.getElementById('btn-play');
 const btnMute = document.getElementById('btn-mute');
-const player = new MediaPlayer({element: video, plugins : [
-    new AutoPlay()
-]});
+const player = new MediaPlayer({
+    element: video, 
+    plugins : [new AutoPlay(), new AutoPause()]
+});
 
 btnPlay.onclick = btnPlay.onclick = () => player.togglePlay();
 btnMute.onclick = btnMute.onclick = () => player.toggleMute();
