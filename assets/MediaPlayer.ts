124d1c62a0bc03392eec82b17205369cdaa4a6dd
Alfredo Morales Sánchez <bluesmoss@gmail.com>
2020-05-25 17:58:03 -0500

    feature(MediaPlayer):  JS to TS

--- assets/MediaPlayer.ts
+++ assets/MediaPlayer.ts
@@ -1,24 +1,18 @@
 
 class MediaPlayer {
+  
+  media: HTMLMediaElement;
+  plugins:  Array<any>;
+
   constructor(config) {
     this.media = config.element;
     this.plugins = config.plugins || [];
-    this._initPlugins();
+    this.initPlugins();
   }
-  _initPlugins() {
-    const player = {
-      play: () => this.play(),
-      pause: () => this.pause(),
-      media: this.media,
-      get muted() {
-        return this.media.muted;
-      },
-      set muted(value) {
-        this.media.muted = value;
-      }
-    };
+  
+  private initPlugins() {
     this.plugins.forEach(plugin => {
-      plugin.run(player);
+      plugin.run(this);
     });
   }
   play() {
@@ -40,12 +34,6 @@ class MediaPlayer {
     (this.media.muted) ? this.unmute() : this.mute();
   }
 }
-  
-
-  
-  
-
-
 
 
   export default MediaPlayer;
--- assets/index.ts
+++ assets/index.ts
@@ -1,10 +1,10 @@
-import MediaPlayer from './MediaPlayer.js';
-import AutoPlay from './plugins/Autoplay.js';
-import AutoPause from './plugins/AutoPause.ts';
+import MediaPlayer from './MediaPlayer';
+import AutoPlay from './plugins/AutoPlay';
+import AutoPause from './plugins/AutoPause';
 
 const video = document.querySelector('video');
-const btnPlay = document.getElementById('btn-play');
-const btnMute = document.getElementById('btn-mute');
+const btnPlay: HTMLElement = document.getElementById('btn-play');
+const btnMute: HTMLElement = document.getElementById('btn-mute');
 const player = new MediaPlayer({
     element: video, 
     plugins : [new AutoPlay(), new AutoPause()]
