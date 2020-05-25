4b18a821f72c182a76c27d9c1928da018affa01f
Alfredo Morales Sánchez <bluesmoss@gmail.com>
2020-05-25 14:37:10 -0500

    feature(Ejercicios): Typescript functions

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -24,6 +24,32 @@ comodin = {type : 'Wildcard'};
 let someObject :  object = {type: 'WildCard'}
 
 
+//functions
+function add(a: number, b:number): number {
+  return a + b;
+}
+
+const sum = add(4, 6);
+
+function createAdder(a: number): (number) => number {
+  return function (b:number) {
+    return b + a;
+  }
+}
+
+const addFour =  createAdder(4);
+const fourPlus6 = addFour(6);
+
+function fullName(firstName:string, lastName:string = 'Smith'): string {
+  return `${firstName} ${lastName}`;
+}
+
+const richard = fullName('Alfred');
+
+
+
+
+
 //interface
 interface Rectangulo {
   ancho: number;
