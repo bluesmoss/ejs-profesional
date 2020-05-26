334944bbf1137fc6ad1e2c9c1b37946b2038143c
Alfredo Morales Sánchez <bluesmoss@gmail.com>
2020-05-26 14:40:12 -0500

    feature(MediaPlayer): Ads plugin

--- assets/MediaPlayer.ts
+++ assets/MediaPlayer.ts
@@ -1,15 +1,24 @@
 
 class MediaPlayer {
-  
+
   media: HTMLMediaElement;
   plugins:  Array<any>;
+  container: HTMLElement;
 
   constructor(config) {
     this.media = config.element;
     this.plugins = config.plugins || [];
+    this.initPlayer();
     this.initPlugins();
   }
-  
+
+  initPlayer() {
+    this.container = document.createElement('div');
+    this.media.parentNode.insertBefore(this.container, this.media);
+    this.container.appendChild(this.media)
+    this.container.style.position = 'relative';
+  }
+
   private initPlugins() {
     this.plugins.forEach(plugin => {
       plugin.run(this);
--- assets/index.ts
+++ assets/index.ts
@@ -1,13 +1,14 @@
 import MediaPlayer from './MediaPlayer';
 import AutoPlay from './plugins/AutoPlay';
 import AutoPause from './plugins/AutoPause';
+import Ads from './plugins/Ads';
 
 const video = document.querySelector('video');
 const btnPlay: HTMLElement = document.getElementById('btn-play');
 const btnMute: HTMLElement = document.getElementById('btn-mute');
 const player = new MediaPlayer({
-    element: video, 
-    plugins : [new AutoPlay(), new AutoPause()]
+    element: video,
+    plugins : [new AutoPlay(), new AutoPause(), new Ads()],
 });
 
 btnPlay.onclick = btnPlay.onclick = () => player.togglePlay();
